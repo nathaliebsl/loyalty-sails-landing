c0da17dbab4a007351758655ef2a0d8d5eb7ed58
nathaliebsl <75685745+nathaliebsl@users.noreply.github.com>
2026-01-11 13:49:07 -0300

    Update Team.tsx
    avatar update

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -1,13 +1,13 @@
 import { Anchor } from "lucide-react";
-import ASL from "@/assets/ALEXLEAL.jpg";
+import { ASL } from "@/assets/ALEXLEAL.jpg";
 
 const Team = () => {
   const teamMembers = [
-    { name: "Alexandre Leal", role: "Skipper/Captain", status: "Current" },
-    { name: "André 'Bochecha' Fonseca", role: "Tactician" },
-    { name: "Gabriel Ribeiro", role: "Trimmer" },
-    { name: "Isaque Soares", role: "Bowman" },
-    { name: "Thiago Knippling", role: "Trimmer" },
+    { name: "Alexandre Leal", role: "Skipper/Captain", status: "Current", avatar: ASL  },
+    { name: "André 'Bochecha' Fonseca", role: "Tactician", avatar: Anchor },
+    { name: "Gabriel Ribeiro", role: "Trimmer", avatar: Anchor },
+    { name: "Isaque Soares", role: "Bowman", avatar: Anchor },
+    { name: "Thiago Knippling", role: "Trimmer", avatar: Anchor },
   ];
 
   return (
@@ -32,7 +32,7 @@ const Team = () => {
               className="group relative bg-card rounded-lg border border-border p-8 text-center hover:border-primary/50 transition-all duration-500 hover:shadow-glow"
             >
               <div className="w-20 h-20 rounded-full bg-gradient-ocean mx-auto mb-6 flex items-center justify-center">
-                <Anchor className="w-8 h-8 text-primary-foreground" />
+                <{member.avatar} className="w-8 h-8 text-primary-foreground" />
               </div>
               <h4 className="text-xl font-bold text-foreground mb-1">
                 {member.name}
