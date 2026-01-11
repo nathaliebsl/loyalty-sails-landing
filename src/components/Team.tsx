40c5dd1856ed7f6fd9a617027fdcbe4b4bbf6ae6
nathaliebsl <75685745+nathaliebsl@users.noreply.github.com>
2026-01-11 13:50:35 -0300

    Update Team.tsx
    avatar error update

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -1,9 +1,9 @@
 import { Anchor } from "lucide-react";
-import { ASL } from "@/assets/ALEXLEAL.jpg";
+import  ASL  from "@/assets/ALEXLEAL.jpg";
 
 const Team = () => {
   const teamMembers = [
-    { name: "Alexandre Leal", role: "Skipper/Captain", status: "Current", avatar: ASL  },
+    { name: "Alexandre Leal", role: "Skipper/Captain", status: "Current", avatar: ASL },
     { name: "André 'Bochecha' Fonseca", role: "Tactician", avatar: Anchor },
     { name: "Gabriel Ribeiro", role: "Trimmer", avatar: Anchor },
     { name: "Isaque Soares", role: "Bowman", avatar: Anchor },
